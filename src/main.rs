485b1afa18214134694db2e87823f75571a072e7
Ken Aaker <kenaaker@gmail.com>
2025-02-05 09:40:58 -0600

    Initial commit new file:   Cargo.toml new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use linux_embedded_hal::I2cdev;
+use veml6030::{SlaveAddr, Veml6030};
+
+fn main() {
+    let dev = I2cdev::new("/dev/play/qwiic/i2c/").unwrap();
+    let address = SlaveAddr::default();
+    let mut sensor = Veml6030::new(dev, address);
+    sensor.enable().unwrap();
+    loop {
+        let lux = sensor.read_lux().unwrap();
+        println!("lux: {:2}", lux);
+    }
+}
