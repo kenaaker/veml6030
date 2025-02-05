a0c7c87a0fcd388e66031ffca3b8f40044e7827c
Kenneth D. Aaker <kenaaker@beagleplay.aaker.org>
2025-02-05 17:53:03 +0000

    Added std::sleep to code to delay for a second between lux readings.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+
+
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
 use linux_embedded_hal::I2cdev;
 use veml6030::{SlaveAddr, Veml6030};
 
@@ -5,9 +10,14 @@ fn main() {
     let dev = I2cdev::new("/dev/play/qwiic/i2c").unwrap();
     let address = SlaveAddr::Alternative(true);
     let mut sensor = Veml6030::new(dev, address);
+    let interval = Duration::from_secs(1);
+    let mut next_time = Instant::now() + interval;
     sensor.enable().unwrap();
     loop {
         let lux = sensor.read_lux().unwrap();
-        println!("lux: {:2}", lux);
+        println!("{:2}", lux);
+
+        sleep(next_time - Instant::now());
+        next_time += interval;
     }
 }
